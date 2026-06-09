c1425566338eb8e5c31a4b13f08987a935bd2d35
JoaoBerton <141670692+JoaoBerton@users.noreply.github.com>
2026-06-09 14:00:52 -0300

    aula11 adicionada.

---
+++ aula11/js/script.js
@@ -0,0 +1,6 @@
+let num1 = prompt("Digite um Número.");
+let num2 = prompt("Digite outro Número.");
+num1 = Number(num1);
+num2 = Number(num2);
+let valor = num1 + num2
+window.alert(`O resultado dos dois números digitados é: ${valor}. Os valores digitados foram: ${num1} & ${num2} respectivamente.`);
